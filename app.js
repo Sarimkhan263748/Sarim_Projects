6b9c89cc5c64a9046d15d1e316d9551322481922
Sarimkhan263748 <sarimkhan0526@gmail.com>
2024-05-11 17:05:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const smallCups = document.querySelectorAll('.cups-small')
+const liters = document.getElementById('liters');
+const percentage = document.getElementById('percentage');
+const remained = document.getElementById('remained');
+
+updateBigcups()
+
+smallCups.forEach((cup, idx) => {
+   cup.addEventListener('click',() => highlightcups(idx))
+})
+
+function highlightCups(idx) {}
+    if(idx ---7 && small (idx).classlist.contalms('full')) idx--;
+    else if(smallCups(idx).classlist.contains('full')&& smallCups(idx).
+nextElementSibling.classlist.contains('full')) (
+    idx--
+)
+
